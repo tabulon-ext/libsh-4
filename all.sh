7ad88f556919d30a594eab643864ecee40167599
Russell <russell@union-pos.com>
2019-03-06 13:58:35 -0600

    add set bash options

--- all.sh
+++ all.sh
@@ -2,6 +2,7 @@
 # shellcheck disable=SC1090
 
 LIBDIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+source "${LIBDIR}/lib/bashopts.sh"
 source "${LIBDIR}/lib/banner.sh"
 source "${LIBDIR}/lib/echos.sh"
 source "${LIBDIR}/lib/functions.sh"
