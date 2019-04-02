be4924cbcecaaaf5a4bd7fb1eb96b61679a4ca88
Russell <russell@union-pos.com>
2019-04-02 10:45:54 -0500

    add logging redirects

--- lib/bashopts.sh
+++ lib/bashopts.sh
@@ -30,3 +30,22 @@ set -o pipefail
 # Make debugging easier when you use `set -x`
 # See: http://wiki.bash-hackers.org/scripting/debuggingtips#making_xtrace_more_useful
 export PS4='+(${BASH_SOURCE}:${LINENO}): ${FUNCNAME[0]:+${FUNCNAME[0]}(): }'
+
+
+# Saves file descriptors so they can be restored to whatever they were before
+# redirection or used themselves to output to whatever they were before the
+# following redirect.
+exec 3>&1 4>&2
+
+# Restore file descriptors for particular signals. Not generally necessary since
+# they should be restored when the sub-shell exits.
+trap 'exec 2>&4 1>&3' 0 1 2 3
+
+# Redirect stdout to file log.out then redirect stderr to stdout.
+# Note that the order is important when you want them going to the same file.
+# stdout must be redirected before stderr is redirected to stdout.
+exec 1>log.out 2>&1
+
+# From then on, to see output on the console (maybe), you can simply redirect to &3. For example,
+# echo "$(date) : part 1 - start" >&3
+# will go to wherever stdout was directed, presumably the console, prior to executing line 3 above.
